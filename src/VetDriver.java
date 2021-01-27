87b29331b716073691b4c6ed562c2979a79a9ee3
Timothy Wilks <91078140@mvusd.net>
2021-01-26 19:55:33 -0800

    finished constructors and made the main method in the driver

--- src/VetDriver.java
+++ src/VetDriver.java
@@ -2,6 +2,6 @@ public class VetDriver
 {
     public static void main(String[] args)
     {
-
+        VetRecords pup = new VetRecords("Dan", "Ken", "Max", true, 1, 15.6, "Dog");
     }
 }
--- src/VetRecords.java
+++ src/VetRecords.java
@@ -2,26 +2,32 @@ public class VetRecords
 {
     public String FName;
     public String LName;
+    public String petName;
     public String species;
     public boolean isMale;
     public int age;
     public double weight;
 
-    public VetRecords(String firstName, String lastName, boolean isMale, int age, double weight, String species)
+    public VetRecords(String firstName, String lastName, String petName, boolean isMale, int age, double weight, String species)
     {
-        FName = firstName;
-        LName = lastName;
-        isMale = isMale;
-        age = age;
+        this.FName = firstName;
+        this.LName = lastName;
+        this.petName = petName;
+        this.isMale = isMale;
+        this.age = age;
+        this.weight = weight;
+        this.species = species;
     }
 
     public VetRecords()
     {
         FName = null;
         LName = null;
+        petName = null;
         isMale = false;
         age = 0;
         weight = 0.0;
+        species = null;
     }
 
 }
