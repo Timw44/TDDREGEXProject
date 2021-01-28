d2d71a26ba7839657cf42c2db51094ecbbb6752d
Timothy Wilks <91078140@mvusd.net>
2021-01-27 16:52:24 -0800

    added print line in driver to tes toString

--- src/VetDriver.java
+++ src/VetDriver.java
@@ -3,5 +3,6 @@ public class VetDriver
     public static void main(String[] args)
     {
         VetRecords pup = new VetRecords("Tim", "Wilks", "Max", true, 10, 25.6, "Dog");
+        System.out.println(pup.toString());
     }
 }
