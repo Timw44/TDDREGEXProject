83bcb4115324db2707dffe7695eab6c426e2b6f1
Timothy Wilks <91078140@mvusd.net>
2021-01-28 17:18:32 -0800

    created tests in driver class

--- src/VetDriver.java
+++ src/VetDriver.java
@@ -4,8 +4,15 @@ public class VetDriver
 {
     public static void main(String[] args)
     {
+        VetRecords pup = new VetRecords();
         Scanner scan = new Scanner(System.in);
-        VetRecords pup = new VetRecords("Tim", "Wilks", "Max", "male", 10, 25.6, "Dog");
-        System.out.println(pup.toString());
+
+        System.out.println("Enter Your First Name: ");
+        pup.setFName(scan.nextLine());
+        System.out.println("Enter Your Last Name: ");
+        pup.setLName(scan.nextLine());
+        System.out.println("Enter Your Pet's Name: ");
+        pup.setPetName(scan.nextLine());
+        pup.toString();
     }//end of main method
 }//end of VetDriver class
