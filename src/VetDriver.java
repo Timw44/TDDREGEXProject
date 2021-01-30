4f0336586d9e5e30e9cf52aee61c603310b6c437
Timothy Wilks <91078140@mvusd.net>
2021-01-29 19:08:24 -0800

    added more output after user input

--- src/VetDriver.java
+++ src/VetDriver.java
@@ -9,12 +9,19 @@ public class VetDriver
 
         System.out.print("Enter Your First Name: ");
         pup.setFName(scan.nextLine());
+
         System.out.print("Enter Your Last Name: ");
         pup.setLName(scan.nextLine());
+
         System.out.print("Enter Your Pet's Name: ");
         pup.setPetName(scan.nextLine());
+
         System.out.print("Enter Your Pet's Gender: ");
         pup.setGender(scan.nextLine());
-        pup.toString();
+
+        System.out.println("First Name: " + pup.getFName());
+        System.out.println("Last Name: " + pup.getLName());
+        System.out.println("Pet's Name: " + pup.getPetName());
+        System.out.println("Pet's Gender: " + pup.getGender());
     }//end of main method
 }//end of VetDriver class
