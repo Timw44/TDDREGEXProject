153d68136596880dded9ae22792cb2fe48ea5c27
Timothy Wilks <91078140@mvusd.net>
2021-02-11 09:14:38 -0800

    added defaultConstructor test

--- TestVet/VetRecordsTest.java
+++ TestVet/VetRecordsTest.java
@@ -4,12 +4,20 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class VetRecordsTest
 {
-@Test
-    public void initialToString()//will need to be changes as more variables are added
-{
+    @Test
+    public void initialToString()//will need to be changed as more variables are added
+    {
     VetRecords dog = new VetRecords("Ty", "Ler", "Max", "Male", 10, 25.6, "Dog");
     String expected = "Owner: Ty, Ler; Pet: Max, Male, 10 years old, 25.6kg, Dog";
     assertEquals(expected, dog.toString());
-}
+    }
+
+    @Test
+    public void defaultConstructor()//will need to be changed as more variables are added
+    {
+        VetRecords dog = new VetRecords();
+        String expected = "Owner: null, null; Pet: null, null, 0 years old, 0.0kg, null";
+        assertEquals(expected, dog.toString());
+    }
 
 }
